56b3074c42514cbe89981e8981c667b3b9f18fa3
mohammadnpak <m.nozaripak@gmail.com>
2023-02-16 12:32:27 +0330

    s09c

--- s09/s09.py
+++ s09/s09.py
@@ -48,67 +48,67 @@
 # print(dir(d1))
 
 
-class Card:
+# class Card:
 
-    _hokm = 0
-    _active_suit = 0
-    _suit2num = {'♥':0,'♦':1,'♣':2,'♠':3}
-    _num2suit = {0:'♥',1:'♦',2:'♣',3:'♠'}
+#     _hokm = 0
+#     _active_suit = 0
+#     _suit2num = {'♥':0,'♦':1,'♣':2,'♠':3}
+#     _num2suit = {0:'♥',1:'♦',2:'♣',3:'♠'}
 
-    def __init__(self,number,suit:int):
-        self.number = number
-        self.suit =suit
+#     def __init__(self,number,suit:int):
+#         self.number = number
+#         self.suit =suit
     
-    @classmethod
-    def set_hokm(cls,hokm):
-        if 0<=hokm<4:
-            cls._hokm = hokm
-        else:
-            raise Exception("hokm must be in 0<= hokm < 4 ")
+#     @classmethod
+#     def set_hokm(cls,hokm):
+#         if 0<=hokm<4:
+#             cls._hokm = hokm
+#         else:
+#             raise Exception("hokm must be in 0<= hokm < 4 ")
     
-    @classmethod
-    def set_active_suit(cls,suit):
-        if 0<=suit<4:
-            cls._active_suit = suit
-        else:
-            raise Exception("hokm must be in 0<= hokm < 4 ")
+#     @classmethod
+#     def set_active_suit(cls,suit):
+#         if 0<=suit<4:
+#             cls._active_suit = suit
+#         else:
+#             raise Exception("hokm must be in 0<= hokm < 4 ")
 
-    @classmethod
-    def number_to_suit(cls,number):
-        return cls._num2suit[number]
+#     @classmethod
+#     def number_to_suit(cls,number):
+#         return cls._num2suit[number]
     
-    @classmethod
-    def suit_to_number(cls,suit):
-        return cls._suit2num[suit]
+#     @classmethod
+#     def suit_to_number(cls,suit):
+#         return cls._suit2num[suit]
 
-    def __gt__(self,other):
-        if self.suit == Card._hokm and other.suit != Card._hokm:
-            return True
-        elif self.suit == Card._hokm and other.suit == Card._hokm:
-            return True if self.number > other.number else False
-            # return self.number > other.number
-        elif self.suit != Card._hokm and other.suit == Card._hokm:
-            return False
-        elif self.suit != Card._hokm and other.suit != Card._hokm:
-            if self.suit == Card._active_suit and other.suit != Card._active_suit:
-                return True
-            elif  self.suit == Card._active_suit and other.suit == Card._active_suit:
-                return self.number > other.number
-            elif  self.suit != Card._active_suit and other.suit == Card._active_suit:
-                return False
-            elif  self.suit != Card._active_suit and other.suit != Card._active_suit:
-                return self.suit > other.suit
+#     def __gt__(self,other):
+#         if self.suit == Card._hokm and other.suit != Card._hokm:
+#             return True
+#         elif self.suit == Card._hokm and other.suit == Card._hokm:
+#             return True if self.number > other.number else False
+#             # return self.number > other.number
+#         elif self.suit != Card._hokm and other.suit == Card._hokm:
+#             return False
+#         elif self.suit != Card._hokm and other.suit != Card._hokm:
+#             if self.suit == Card._active_suit and other.suit != Card._active_suit:
+#                 return True
+#             elif  self.suit == Card._active_suit and other.suit == Card._active_suit:
+#                 return self.number > other.number
+#             elif  self.suit != Card._active_suit and other.suit == Card._active_suit:
+#                 return False
+#             elif  self.suit != Card._active_suit and other.suit != Card._active_suit:
+#                 return self.suit > other.suit
         
 
-    def __lt__(self,other):
-        pass
+#     def __lt__(self,other):
+#         pass
     
 
-print('❤️')
-a = Card(2,0)
-b = Card(5,2)
-a.set_hokm(0)
-a>b
+# print('❤️')
+# a = Card(2,0)
+# b = Card(5,2)
+# a.set_hokm(0)
+# a>b
 # print(Card.suit_to_number('♥'))
 
 
@@ -124,3 +124,26 @@ a>b
 
 # sample_def = sample_decorator(sample_def)
 
+
+class Father:
+    def __init__(self,name) -> None:
+        self.name = name
+        self.gender = "male"
+
+class Mother:
+    def __init__(self,name) -> None:
+        self.name = name
+        self.gender = "female"
+
+class Student(Father,Mother):
+    def __init__(self, n,id) -> None:
+        self.student_id = id
+        super(Mother).__init__(n)
+
+p1 = Father("mohamad")
+p2 = Mother("mahdieh")
+s = Student("ali",123)
+
+print(p1.__dict__)
+print(p2.__dict__)
+print(s.__dict__)
